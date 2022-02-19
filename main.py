c8df39c45ddd631a3a2aa650412adc1b3fae971b
Berkin Anık <berkinanik@gmail.com>
2022-02-19 23:35:01 +0300

    bugfix on char replacing | example links

--- main.py
+++ main.py
@@ -7,36 +7,39 @@ from tools.extract_links_from_pdf import extractLinksFromPDF
 # all youtube playlist and video urls must lead to public or unlisted youtube content
 # if you are trying to reach a private youtube playlist or video wont work as expected
 
+
 def main():
     # DOWNLOAD VIDEOS FROM YOUTUBE PLAYLIST
     # use following line to extract youtube videos from a youtube playlist
-    
-    #TODO edit following url for your platlist
-    
+
+    # TODO edit following url for your platlist
+
+    videos = Playlist("https://www.youtube.com/playlist?list=[playlist-ID]")
+    # example
     videos = Playlist(
-        "https://www.youtube.com/playlist?list=[playlist-ID]"
+        "https://www.youtube.com/playlist?list=PLEboAksnoN7UtpR7gJzCsZFCPbOo3ur0s"
     )
 
-
     # download path of videos to be downloaded to
-    
-    #TODO edit following string for where videos to be downloaded to
-    
+
+    # TODO edit following string for where videos to be downloaded to
+
     # use following line for windows
-    
+
     downloadPath = "C:\\Path\\To\\Your\\Download\\Folder\\"
-    # downloadPath = "C:\\Users\\Berkin\\Desktop\\YoutubeDownloads\\example-folder\\"
-    
+    # example
+    downloadPath = "C:\\Users\\Berkin\\Desktop\\YoutubeDownloads\\ee302-vids\\"
+
     # use following line for macOS or linux
-    
+
     # downloadPath = "/path/to/your/download/folder/"
 
+    # file name prefix to be added all downloaded videos
+    # TODO edit following string for file name prefix
 
-    #file name prefix to be added all downloaded videos
-    #TODO edit following string for file name prefix
-    
     fileNamePrefix = "file-name-prefix"
-
+    # example
+    fileNamePrefix = "ee302-2020f"
 
     # EXTRACT YOUTUBE LINKS FROM PDF
     # you can extract youtube links from pdf and you the array returned to download youtube videos
@@ -46,29 +49,27 @@ def main():
     # if you have a pdf that contains clickable valid urls of youtube links you can use this
     # there is a validation regex used for validating youtube vid links
     # it is not guaranteed to be working flawlessly
-    
-    #TODO uncomment following line and edit pdf path to extract videos from pdf
-    
+
+    # TODO uncomment following line and edit pdf path to extract videos from pdf
+
     # Windows
-    
+
     # videos = extractLinksFromPDF("C:\\Path\\To\\example-pdf-containing-youtube-video-url-anchors.pdf")
     # videos = extractLinksFromPDF("C:\\Users\\Berkin\\Desktop\\link-pdf.pdf")
-    
-    # MacOS or Linux
-    
-    # videos = extractLinksFromPDF("/path/to/example-pdf-containing-youtube-video-url-anchors.pdf")
 
+    # MacOS or Linux
+
+    # videos = extractLinksFromPDF("/path/to/example-pdf-containing-youtube-video-url-anchors.pdf")
 
     # CREATE YOUR OWN LIST OF VIDEOS
     # you might use this type of an array of youtube video links to download
-    
-    #TODO uncomment following lines and create your own array of youtube vid links
-    
+
+    # TODO uncomment following lines and create your own array of youtube vid links
+
     # videos = [
     #     "https://www.youtube.com/watch?v=[video-id1]",
     #     "https://www.youtube.com/watch?v=[video-id2]",
     # ]
-
 
     # counter of videos downloaded
     # you might change this to number whether you want counter index to be start from
@@ -86,18 +87,18 @@ def main():
             fileNamePrefix
             + f"-{'0'+str(count) if count < 10 else count}-"
             + result.title.lower()
-            .replace("middle east technical university", "") #remove words you want as lower case from name 
-            .replace("removethisonealso", "") 
-            .replace("2022", "") #numbers also
-            .replace("metu", "odtu") #or replace
+            .replace("ee302", "")
+            .replace("feedback systems", "")
+            .replace("(Lectures on Feedback Control Systems)".lower(), "")
+            .replace("spring 2021", "")
             + ".mp4"
         )
-        invalidChars = '<>:;_.,"/\|?* '
+        invalidChars = '<>:;_,"/\|?* '
         # removing invalids chars from name and replacing with -
         for char in invalidChars:
             fileName = fileName.replace(char, "-")
         # remove dash repetition
-        fileName = re.sub('\-+', '-', fileName)
+        fileName = re.sub("\-+", "-", fileName)
         print(fileName)
         result.download(downloadPath, fileName)
         count += 1
@@ -105,6 +106,7 @@ def main():
 
 # callback to follow downloading progress
 previousProgress = 0
+
 
 def on_progress(stream, chunk, bytesRemaining):
     global previousProgress
